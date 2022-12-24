43f55b91f42de81dd2132e13ae5073d34bd2fa71
gauravgorde <gmgorde@gmail.com>
2022-12-24 19:34:40 +0530

    Added sop

--- src/com/sample/Sample.java
+++ src/com/sample/Sample.java
@@ -7,10 +7,11 @@ public class Sample {
 
 	public static void main(String[] args) {
 		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
-		System.out.println(list);
 
 		for (Integer integer : list) {
 			System.out.println(integer);
 		}
+
+		System.out.println("Hello");
 	}
 }
