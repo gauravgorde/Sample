03dbd9db7f7f97678d3e611284dc2764f9014f5d
gauravgorde <gmgorde@gmail.com>
2022-12-26 16:15:19 +0530

    entities added

--- src/com/sample/Sample.java
+++ src/com/sample/Sample.java
@@ -1,8 +1,5 @@
 package com.sample;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class Sample {
 
 	public static void main(String[] args) {
--- src/com/sample/User.java
+++ src/com/sample/User.java
@@ -2,4 +2,7 @@ package com.sample;
 
 public class User {
 
+	int id;
+	String name, city;
+
 }
