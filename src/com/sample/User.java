1ade087be6093c820b767ec5274891ef6a4440b0
gauravgorde <gmgorde@gmail.com>
2022-12-26 16:19:11 +0530

    getter and setter added

--- src/com/sample/User.java
+++ src/com/sample/User.java
@@ -5,4 +5,45 @@ public class User {
 	int id;
 	String name, city;
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", city=" + city + "]";
+	}
+
+	public User(int id, String name, String city) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.city = city;
+	}
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
 }
