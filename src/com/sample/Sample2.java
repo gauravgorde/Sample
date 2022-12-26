e3229609d192e13aaeea4857928560e5a2611e36
gauravgorde <gmgorde@gmail.com>
2022-12-26 10:49:20 +0530

    Sample 2 added

---
+++ src/com/sample/Sample2.java
@@ -0,0 +1,12 @@
+package com.sample;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+		System.out.println(list);
+	}
+}
