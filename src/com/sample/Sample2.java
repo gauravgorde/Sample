dddd5ae52a8cb3dc7052c16caf442b375cdfeabd
gauravgorde <gmgorde@gmail.com>
2022-12-26 12:07:29 +0530

    Set code added

--- src/com/sample/Sample2.java
+++ src/com/sample/Sample2.java
@@ -1,15 +1,13 @@
 package com.sample;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.Set;
 
 public class Sample2 {
 
 	public static void main(String[] args) {
-		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
-		System.out.println(list);
-		
-		list.forEach(System.out::println);
+
+		Set<Integer> set = Set.of(1, 22, 33, 55, 44);
+		System.out.println(set);
 
 	}
 }
