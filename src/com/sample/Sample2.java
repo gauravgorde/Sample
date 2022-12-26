29609c73890b7e386bf61303a1345cd3a3fe86f6
gauravgorde <46414147+gauravgorde@users.noreply.github.com>
2022-12-26 12:03:27 +0530

    extra unwanted code removed

--- src/com/sample/Sample2.java
+++ src/com/sample/Sample2.java
@@ -8,18 +8,8 @@ public class Sample2 {
 	public static void main(String[] args) {
 		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
 		System.out.println(list);
-
-
+		
 		list.forEach(System.out::println);
-		
-		
-		for (Integer integer : list) {
-			System.out.println(integer);
-		}
-		
-		
-		for (Integer integer : list) {
-			System.out.println(integer);
-		}
+
 	}
 }
